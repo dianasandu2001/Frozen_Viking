6c5261b9226f025544d670a0e04717ebdce32f45
Diana Sandu <142903238+dianasandu2001@users.noreply.github.com>
2024-09-05 14:24:46 +0300

    some code, walking jumping and idleing

---
+++ Frozen_Viking/Assets/Scripts/Character_Control.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Character_Control : MonoBehaviour
+{
+    public float move_speed;
+    public float jump_force;
+
+    public Animator animator;
+    public Rigidbody2D rb2D;
+
+    public Transform ground_check_position;
+    public float ground_check_radius;
+    public LayerMask ground_check_layer;
+    public bool grounded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.OverlapCircle(ground_check_position.position, ground_check_radius, ground_check_layer);
+        transform.Translate(Input.GetAxis("Horizontal") * move_speed * Time.deltaTime, 0, 0);
+        if (Input.GetAxisRaw("Horizontal") != 0 )
+        {
+            // This means you either have a or d pressed
+            transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1);
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            //Nothing is pressed
+            animator.SetBool("Walk", false);
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            rb2D.velocity = new Vector2(0, jump_force);
+            animator.SetTrigger("Jump");
+        }
+    }
+}
